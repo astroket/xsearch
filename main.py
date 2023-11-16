423cbbb322d1d167e96ba3b4b770e377c1973099
Ketan Chandra <ketan.iitm@gmail.com>
2023-11-16 11:43:17 +0530

    all files at base

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__=="__main__":
-    uvicorn.run("src.endpoints:app",host="127.0.0.1",port=9009,reload=True)
+    uvicorn.run("endpoints:app",host="127.0.0.1",port=9009,reload=True)
